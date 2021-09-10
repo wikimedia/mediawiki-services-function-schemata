d0c7e358e61b020c96aa348409bac56d5b4f3371
apine <cmassaro@wikimedia.org>
2021-09-10 16:12:21 -0500

    Add more fine-grained support for canonical ZObjects
    Bug: T290741
    Change-Id: Ib1fe588b39aa5f7e888b6c40f78d56e245ba64f8

--- php/test/integration/CanonicalZObjectTest.php
+++ php/test/integration/CanonicalZObjectTest.php
@@ -5,25 +5,25 @@ namespace Mediawiki\Services\Wikilambda\FunctionSchemata\Tests;
 use Mediawiki\Services\Wikilambda\FunctionSchemata\SchemaFactory;
 use Mediawiki\Services\Wikilambda\FunctionSchemata\SchemataUtils;
 
-final class NormalZObjectTest extends ValidationTest {
+final class CanonicalZObjectTest extends ValidationTest {
 	/**
 	 * @coversNothing
 	 * @dataProvider provideZIDs
 	 */
-	public function testNormalizedZObject( $ZID ): void {
-		$validator = ( SchemaFactory::getNormalFormFactory() )->create( $ZID );
-		$normalFile = SchemataUtils::joinPath(
-			SchemataUtils::testDataDirectory(), "normal_zobject", $ZID . ".yaml"
+	public function testCanonicalizedZObject( $ZID ): void {
+		$validator = ( SchemaFactory::getCanonicalFormFactory() )->create( $ZID );
+		$canonalFile = SchemataUtils::joinPath(
+			SchemataUtils::testDataDirectory(), "canonical_zobject", $ZID . ".yaml"
 		);
-		$testDescriptor = json_decode( SchemataUtils::readYamlAsSecretJson( $normalFile ) );
+		$testDescriptor = json_decode( SchemataUtils::readYamlAsSecretJson( $canonalFile ) );
 		$this->testValidation( $validator, $testDescriptor->test_objects );
 	}
 
 	public function provideZIDs() {
-		// Keep in lock-step with CanonicalZObjectTest::provideZIDs()
+		// Keep in lock-step with NormalZObjectTest::provideZIDs()
 		// TODO: Enable test for Z7.
 		return [
-			[ "Z1" ], [ "Z2" ], [ "Z7_backend" ], [ "Z10" ],
+			[ "Z1" ], [ "Z2" ], [ "Z6" ], [ "Z7_backend" ], [ "Z10" ],
 			[ "Z14" ], [ "Z17" ], [ "Z18" ], [ "Z22" ], [ "Z39" ], [ "Z40" ],
 			[ "Z61" ], [ "Z80" ], [ "Z86" ], [ "Z99" ]
 		];
