6a8eb2949bade970859a3493d6216240e665120a
apine <cmassaro@wikimedia.org>
2022-03-17 09:44:49 -0500

    Normalize() should produce generic Typed Lists by default.
    Change-Id: I1f965f26486069e0fdfa7ff5fa434739804daebb

--- javascript/test/unit/src/normalizeTest.js
+++ javascript/test/unit/src/normalizeTest.js
@@ -12,17 +12,17 @@ QUnit.module( 'normalization' );
 	const testPath = path.join( 'test_data', 'normalization.yaml' );
 	const testDescriptor = readYaml( testPath );
 	const info = testDescriptor.test_information;
+	const normalizeNonGenerically = ( ZObject ) => normalize( ZObject, /* generically= */false );
 
 	// eslint-disable-next-line qunit/no-test-expect-argument
-	test( info.name, normalize, testDescriptor.test_objects ).then();
+	test( info.name, normalizeNonGenerically, testDescriptor.test_objects ).then();
 }
 
 {
 	const testPath = path.join( 'test_data', 'normalization_generic.yaml' );
 	const testDescriptor = readYaml( testPath );
 	const info = testDescriptor.test_information;
-	const normalizeGenerically = ( ZObject ) => normalize( ZObject, /* generically= */true );
 
 	// eslint-disable-next-line qunit/no-test-expect-argument
-	test( info.name, normalizeGenerically, testDescriptor.test_objects ).then();
+	test( info.name, normalize, testDescriptor.test_objects ).then();
 }
