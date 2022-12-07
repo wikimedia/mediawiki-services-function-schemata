b8cc8183e38c8c5779b351e74ae6226474ebec55
James D. Forrester <jforrester@wikimedia.org>
2022-12-07 12:56:57 -0500

    error: Drop normalError() and canonicalError(), deprecated and unused
    Bug: T305153
    Depends-On: Ia7d8bb4198372e7ae232247e3926be5ea2cc2120
    Change-Id: Ic1ba2f1c128b0be796f62394b71958216f0f261d

--- javascript/test/integration/error_test.js
+++ javascript/test/integration/error_test.js
@@ -2,7 +2,6 @@
 
 const path = require( 'path' );
 const { SchemaFactory } = require( '../../src/schema.js' );
-const { canonicalError, normalError } = require( '../../src/error.js' );
 const { readYaml } = require( '../../src/fileUtils.js' );
 const { testErrors } = require( '../testUtils.js' );
 
@@ -28,38 +27,7 @@ function testValidateCanonical( ZID ) {
 	testErrors( info.name, canonicalValidator, testDescriptor.test_objects, errorValidator );
 }
 
-function testCreateCanonical() {
-	QUnit.test( 'canonicalError', ( assert ) => {
-		const simpleStringError = canonicalError( [ 'Z507' ], [ 'Extra data' ] );
-		assert.deepEqual( simpleStringError, { Z1K1: 'Z5', Z5K1: { Z1K1: 'Z507', Z507K1: 'Extra data' } } );
-
-		const doubleStringError = canonicalError( [ 'Z507' ], [ 'Extra data', 'More extra data' ] );
-		assert.deepEqual( doubleStringError, { Z1K1: 'Z5', Z5K1: { Z1K1: 'Z507', Z507K1: 'Extra data', Z507K2: 'More extra data' } } );
-
-		const nestedStringError = canonicalError( [ 'Z507', 'Z509' ], [ 'Extra data' ] );
-		assert.deepEqual( nestedStringError, { Z1K1: 'Z5', Z5K1: { Z1K1: 'Z507', Z507K1: { Z1K1: 'Z509', Z509K1: 'Extra data' } } } );
-	} );
-}
-
-function testCreateNormal() {
-	QUnit.test( 'normalError', ( assert ) => {
-		const simpleStringError = normalError( [ 'Z507' ], [ 'Extra data' ] );
-		assert.deepEqual( simpleStringError, { Z1K1: { Z1K1: 'Z9', Z9K1: 'Z5' }, Z5K1: { Z1K1: { Z1K1: 'Z9', Z9K1: 'Z507' }, Z507K1: { Z1K1: 'Z6', Z6K1: 'Extra data' } } } );
-
-		const doubleStringError = normalError( [ 'Z507' ], [ 'Extra data', 'More extra data' ] );
-		assert.deepEqual( doubleStringError, { Z1K1: { Z1K1: 'Z9', Z9K1: 'Z5' }, Z5K1: { Z1K1: { Z1K1: 'Z9', Z9K1: 'Z507' }, Z507K1: { Z1K1: 'Z6', Z6K1: 'Extra data' }, Z507K2: { Z1K1: 'Z6', Z6K1: 'More extra data' } } } );
-
-		const nestedStringError = normalError( [ 'Z507', 'Z509' ], [ 'Extra data' ] );
-		assert.deepEqual( nestedStringError, { Z1K1: { Z1K1: 'Z9', Z9K1: 'Z5' }, Z5K1: { Z1K1: { Z1K1: 'Z9', Z9K1: 'Z507' }, Z507K1: { Z1K1: 'Z509', Z509K1: { Z1K1: 'Z6', Z6K1: 'Extra data' } } } } );
-
-		const simpleNonStringError = normalError( [ 'Z507' ], [ { Z1K1: { Z1K1: 'Z9', Z9K1: 'Z40' }, Z40K1: { Z1K1: 'Z9', Z9K1: 'Z41' } } ] );
-		assert.deepEqual( simpleNonStringError, { Z1K1: { Z1K1: 'Z9', Z9K1: 'Z5' }, Z5K1: { Z1K1: { Z1K1: 'Z9', Z9K1: 'Z507' }, Z507K1: { Z1K1: { Z1K1: 'Z9', Z9K1: 'Z40' }, Z40K1: { Z1K1: 'Z9', Z9K1: 'Z41' } } } } );
-	} );
-}
-
 QUnit.module( 'ERRORS', () => {
 	testValidateNormal( 'Z2' );
 	testValidateCanonical( 'Z2' );
-	testCreateNormal();
-	testCreateCanonical();
 } );
